0b61c4205a18c46e2fca5743c13de3d0dc797b36
Tongxu <57761655+Tongxulee@users.noreply.github.com>
2022-06-22 00:22:32 +0800

    测试可循环计算文件的hash值

--- crypto/main.c
+++ crypto/main.c
@@ -4,29 +4,37 @@
 #include "./aes/aes_crypto.h"
 #include "./sha/sha2.h"
 
-#define MAX_NUM_A_LINE	60u
+#define MAX_NUM_A_LINE	(1024)
 int main(int argc , char const* argv[])
 {
-	char encrypo_data[SHA256_DIGEST_LENGTH];
-	char buff[100] = {0};
-	char tmpbuff[60] = {0};
-	char position = 0u;
-	FILE* fp = fopen("./tmp.hex" , "r");
-	while (fgets(tmpbuff , MAX_NUM_A_LINE , fp) != NULL)
+	uint8_t encrypo_data[SHA256_DIGEST_LENGTH];
+	uint8_t buff[MAX_NUM_A_LINE] = {0};
+	uint8_t tmpbuff[60] = {0};
+	uint8_t position = 0u;
+	uint16_t i = 0;
+	SHA256_CTX c;
+	FILE* fp = fopen("./tmp.hex" , "rb");
+	FILE* fp2 = fopen("./FVC2cal.hex" , "rb");
+	SHA256_Init(&c);
+
+	while (!feof(fp2))
 	{
-		uint8_t len = strlen(tmpbuff);
-		memcpy(&buff[position] , tmpbuff , len);
-		position += len;
+		unsigned int nCount = fread(buff , 1 , MAX_NUM_A_LINE , fp2);
+		if (ferror(fp2))
+		{
+			printf("\nAn error occurred when accessing the file\n");
+			fclose(fp2);
+			return 0;
+		}
+		SHA256_Update(&c , buff , nCount);
+		printf("count: %d  readbyte: %d\n" , i++ , nCount);
 	}
-	fclose(fp);
+	printf("hash dowm!\n");
+	SHA256_Final(encrypo_data , &c);
+	printf("hash final!\n");
+	memset(&c , 0 , sizeof(c));
+	fclose(fp2);
 	//printf("%s\n" , buff);
-	for (uint8_t i = 0;i < MAX_NUM_A_LINE;i++)
-	{
-		printf("%X" , buff[i]);
-	}
-	printf("\n");
-	printf("%s" , buff);
-	SHA256(buff , 46 , encrypo_data);
 	for (uint8_t i = 0;i < SHA256_DIGEST_LENGTH;i++)
 	{
 		printf("%X" , encrypo_data[i]);
