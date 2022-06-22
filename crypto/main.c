4e3cfe29ed0684a39e575a10de50ffbd5db58e17
Tongxu <57761655+Tongxulee@users.noreply.github.com>
2022-06-23 00:31:59 +0800

    C语言调用aes cbc计算结果，同python和在线验证工具一致

--- crypto/main.c
+++ crypto/main.c
@@ -5,6 +5,9 @@
 #include "./sha/sha2.h"
 
 #define MAX_NUM_A_LINE	(1024)
+
+void AES_CBC_Test(void);
+
 int main(int argc , char const* argv[])
 {
 	uint8_t encrypo_data[SHA256_DIGEST_LENGTH];
@@ -39,6 +42,27 @@ int main(int argc , char const* argv[])
 	{
 		printf("%X" , encrypo_data[i]);
 	}
+	AES_CBC_Test();
 	/* code */
 	return 0;
+}
+
+void AES_CBC_Test(void)
+{
+	uint8_t key[16] = {0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x61,0x62,0x63,0x64,0x65,0x66};
+	uint8_t iv[16] = {0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30};
+
+	uint8_t* msg = "0123456789abcdef";
+	uint8_t encrypo_data[SHA256_DIGEST_LENGTH];
+	uint8_t encrypt_MAC[16];
+	int MAClen = 16;
+	SHA256(msg , 16 , encrypo_data);
+	aes_encrypt_cbc(key , 16 , iv , msg , 16 , encrypt_MAC , &MAClen);
+	printf("\n**********CBC Test start**********\n");
+	//after encrypto maclen is set to 32
+	for (uint8_t i = 0; i < 16;i++)
+	{
+		printf("%X" , encrypt_MAC[i]);
+	}
+	printf("\n**********CBC Test end**********\n");
 }
